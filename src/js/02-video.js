a2adc2ed4f4f457671ff4abc9831acfbf819d983
Andrew <bachynckyi.andrii@gmail.com>
2022-07-21 08:56:09 +0300

    add_task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iFrame = document.querySelector('#vimeo-player');
+const player = new Player(iFrame);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const onPlay = function (data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+if (savedTime) {
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
 
