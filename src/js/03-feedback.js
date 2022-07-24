8d2de748a8d58ceb635e46ab4ef68b4233f5b2e8
Andrew <bachynckyi.andrii@gmail.com>
2022-07-24 07:43:09 +0300

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+onUpdate();
+
+let formData = {}; 
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+
+function onSubmit(event) {
+    event.preventDefault();
+    if (form.elements.email.value === "" || form.elements.message.value=== "") {
+        alert("Поля не могут быть пустыми");
+    }
+    else {
+        console.log(formData);
+    };
+    event.currentTarget.reset();
+    localStorage.clear();
+};
+
+
+function onUpdate() {
+    const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedData) {
+    form.elements.email.value = savedData.email || '';
+    form.elements.message.value = savedData.message || '';
+    };
+};
+
 
